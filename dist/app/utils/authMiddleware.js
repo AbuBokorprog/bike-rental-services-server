f4c428c8c0289f6cce7df3c49f8f1a357dec4736
AbuBokorprog <abubokor1066@gmail.com>
2024-06-14 00:31:27 +0600

    decoded issue fixed

--- dist/app/utils/authMiddleware.js
+++ dist/app/utils/authMiddleware.js
@@ -7,12 +7,8 @@ exports.auth = void 0;
 const catch_async_1 = require("./catch.async");
 const AppError_1 = require("../errors/AppError");
 const http_status_1 = __importDefault(require("http-status"));
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const config_1 = __importDefault(require("../config"));
-// import { TJwtPayload } from '../modules/auth/auth.constants';
-// import { TJWTPayload } from '../modules/auth/auth.constants';
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 const auth = (...RequireRoles) => {
     return (0, catch_async_1.catchAsync)(async (req, res, next) => {
         const token = req.headers.authorization;
@@ -21,18 +17,20 @@ const auth = (...RequireRoles) => {
             throw new AppError_1.AppError(http_status_1.default.UNAUTHORIZED, 'You are unauthorized!');
         }
         // check is the token verify?
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         jsonwebtoken_1.default.verify(token, config_1.default.jwt_secret, (err, decoded) => {
             if (err) {
-                return next(new AppError_1.AppError(401, 'You are unauthorized! Invalid token.'));
+                throw new AppError_1.AppError(401, 'You are unauthorized! Invalid token.');
             }
             const payload = decoded;
             // Checking the payload type
-            if (!payload.data.email || !payload.data.role) {
-                return next(new AppError_1.AppError(401, 'Token is missing required fields.'));
+            if (!payload.email || !payload.role) {
+                throw new AppError_1.AppError(401, 'You are unauthorized! Invalid token.');
+            }
+            if (RequireRoles && !RequireRoles.includes(payload.role)) {
+                throw new AppError_1.AppError(401, 'You are unauthorized!.');
             }
             // Set the decoded payload to req.user
-            req.user = payload.data;
+            req.user = payload;
             next();
         });
     });
--- src/app/utils/authMiddleware.ts
+++ src/app/utils/authMiddleware.ts
@@ -1,15 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from './catch.async';
 import { AppError } from '../errors/AppError';
 import status from 'http-status';
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
-// import { TJwtPayload } from '../modules/auth/auth.constants';
-// import { TJWTPayload } from '../modules/auth/auth.constants';
 
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 export const auth = (...RequireRoles: (string | undefined)[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
@@ -20,21 +15,25 @@ export const auth = (...RequireRoles: (string | undefined)[]) => {
     }
 
     // check is the token verify?
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
     jwt.verify(token, config.jwt_secret, (err, decoded) => {
       if (err) {
-        return next(new AppError(401, 'You are unauthorized! Invalid token.'));
+        throw new AppError(401, 'You are unauthorized! Invalid token.');
       }
 
       const payload = decoded as JwtPayload;
 
       // Checking the payload type
-      if (!payload.data.email || !payload.data.role) {
-        return next(new AppError(401, 'Token is missing required fields.'));
+      if (!payload.email || !payload.role) {
+        throw new AppError(401, 'You are unauthorized! Invalid token.');
+      }
+
+      if (RequireRoles && !RequireRoles.includes(payload.role)) {
+        throw new AppError(401, 'You are unauthorized!.');
       }
 
       // Set the decoded payload to req.user
-      req.user = payload.data;
+      req.user = payload;
 
       next();
     });
